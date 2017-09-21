fce4f2b1ac84e8a299d85374a64b6de1f76dd4eb
Michael Smith <mjsmitths@gmail.com>
2017-09-21 17:41:35 -0400

    pet

---
+++ src/virtualpetshelter/VirtualPet.java
@@ -0,0 +1,14 @@
+package virtualpetshelter;
+
+public class VirtualPet {
+
+	private String petName;
+	
+	private String petDesc;
+	
+	
+public VirtualPet(String petName, String petDesc) {
+	this.petName = petName;
+	this.petDesc = petDesc;
+}
+}
